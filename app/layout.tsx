4a33eefb038108d027c6c2e686b7dbdfdba47b4f
Mohamed Tobal <mohamedtobal369@gmail.com>
2024-01-27 20:32:19 +0200

    UP

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
 import Header from "@/components/Header";
 import "./globals.css";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 
 export const metadata = {
-  title: "Flexibble",
-  description: "Showcase and discover remarkable developer projects",
+  title: "MUI APP",
 }
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
-import Link from 'next/link'
 import React from 'react'
+import { Button, IconButton, MenuItem, Typography } from '@mui/material'
+import Container from '@mui/material/Container';
+import MarkunreadIcon from '@mui/icons-material/Markunread';
+import { Delete, Menu } from '@mui/icons-material';
 
 const Home = () => {
     return (
-        <section className="flex-start flex-col mb-16">
-            <h1>Categories</h1>
-            <h1><Link href='todos'>Todos</Link></h1>
-            <h1>Load More</h1>
-        </section>
+        <div className="home">
+            <Container maxWidth="xl">
+                <Typography component={'h4'} variant='h4' color='#ff5b5b'>Mohamed Tobal</Typography>
+                <Typography noWrap component={'p'} variant='body1' color='#333'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime voluptas corrupti vel expedita dignissimos dicta doloremque nesciunt architecto est omnis vitae eos, incidunt nisi odio voluptatem at quisquam quod repudiandae?</Typography>
+                <MarkunreadIcon/>
+                <Button endIcon={<Delete/>} variant='contained' color='error' className='bg-[tomato]'>Delete</Button>
+                <IconButton size='large'><Menu /></IconButton>
+            </Container>
+        </div>
     )
 }
 
